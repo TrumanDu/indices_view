84435cb10e9cec5e42a43aac27adf5e86d52f556
Truman Du <Truman.P.Du@newegg.com>
2018-07-20 16:23:12 +0800

    optimize page/logo and update requestTimeout to 120000

--- server/routes/server.js
+++ server/routes/server.js
@@ -4,12 +4,13 @@ export default function (server) {
   const client = new elasticsearch.Client({
     host: config.get('elasticsearch.url'),
     //log: 'trace'
+    requestTimeout:120000
   });
   server.route({
     path: '/api/indices_view/_stats',
     method: 'GET',
     handler(req, reply) {
-      client.indices.stats({ human:true }, function (err,response) {
+      client.indices.stats({ human:true,fields:['docs','store','get','search','query_cache'] }, function (err,response) {
         reply(
                response
               );
